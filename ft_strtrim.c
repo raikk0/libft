36465eaa23904b33595d9d669e245ab33b5696eb
raikk0 <thiagodslr@hotmail.com>
2021-06-17 19:10:51 -0300

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,22 +6,32 @@
 /*   By: throdrig <throdrig@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/30 22:03:40 by throdrig          #+#    #+#             */
-/*   Updated: 2021/06/03 05:14:49 by throdrig         ###   ########.fr       */
+/*   Updated: 2021/06/17 19:09:56 by throdrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+*	Allocates (with malloc(3)) and returns a copy of ’s1’ with the characters specified in ’set’ removed from the beginning and the end of the string.
+*/
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	i;
 
-	if (!s1 || !set)
+	if (!s1 || !set) // verificação dos parâmetros passados
 		return (0);
-	while (*s1 && ft_strchr(set, *s1))
+	while (*s1 && ft_strchr(set, *s1)) // percorre s1 até o fim ou não encontrar set em s1.
 		s1++;
-	i = ft_strlen(s1);
-	while (i && ft_strchr(set, s1[i]))
+	i = ft_strlen(s1); // pega o índice do final da string
+	while (i && ft_strchr(set, s1[i])) // percorre s1 do fim até não encontrar set.
 		i--;
 	return (ft_substr(s1, 0, i + 1));
 }
+
+// &&&farofa&&&
+// farofa&&& [i = 9]
+// i = 6
+// farofa
+// ft_substr(s1, 0, 7)
