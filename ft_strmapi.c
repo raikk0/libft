82c5bdd242c19f3d9c26c23c586f13ef6037a69a
raikk0 <thiagodslr@hotmail.com>
2021-06-16 21:26:48 -0300

    Update ft_strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,25 +6,41 @@
 /*   By: throdrig <throdrig@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/25 01:12:21 by throdrig          #+#    #+#             */
-/*   Updated: 2021/06/08 04:29:00 by throdrig         ###   ########.fr       */
+/*   Updated: 2021/06/16 21:25:18 by throdrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
+/*
+*	Applies the function ’f’ to each character of the string ’s’ to create a new string (with malloc(3)) resulting from successive applications of ’f’.
+*	ex: a função 'f' pode transformar os caracteres em maiúsculo, minúsculo, fazer condições, etc... vai da sua imaginação
+*	declarar na main.c:
+*	
+*	char f(unsigned int i, char c)
+*	{
+*		return (ft_toupper(c + (i-i))); // a soma "c + (i-i)"  server somente para o compilador não encanar de não ter usado o parâmetro 'i'.
+*	}
+*
+*	char string[] = "Meu pastel é mais barato";
+*	printf("%s", ft_strmapi(string, &f));
+*
+*/
+
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	char			*new_string;
+	char			*new_string; // string auxiliar que será retornada
 	unsigned int	i;
 
 	i = 0;
-	new_string = ft_strdup(s);
-	if (!new_string)
+	new_string = ft_strdup(s); // duplica a string original
+	if (!new_string) // verificação se correu tudo bem na duplicação
 		return (0);
-	while (new_string[i])
+	while (new_string[i]) 
 	{
-		new_string[i] = (*f)(i, new_string[i]);
+		// percorre a nova string aplicando a função em cada posição da função, passando 'i' como um possível índice para alguma utilidade na função.
+		new_string[i] = (*f)(i, new_string[i]); 
 		i++;
 	}
 	new_string[i] = '\0';
